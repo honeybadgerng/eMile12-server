6dbe17178abb469d1010019594082a9cd3043f3c
Moshood Raji <124494961+honeybadgerng@users.noreply.github.com>
2023-11-12 21:20:02 +0100

    clean all to clean render server error

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,55 +1,9 @@
 "use strict";
-const paystack = require("paystack")(process.env.PAYSTACK_SECRET_KEY);
+
 /**
  * order controller
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::order.order", ({ strapi }) => ({
-  async create(ctx) {
-    const { products, email } = ctx.request.body;
-
-    const userName = "test";
-    try {
-      //retrieve item information
-      const lineItems = await Promise.all(
-        products.map(async (product) => {
-          const item = await strapi
-            .service("api::item.item")
-            .findOne(product.id);
-
-          return {
-            price_data: {
-              currency: "ngn",
-              product_data: {
-                name: item.name,
-              },
-              unit_amount: item.price * 100,
-            },
-            quantity: product.count,
-          };
-        })
-      );
-      // create a paystack session
-      const session = await paystack.checkout.sessions.create({
-        payment_method_types: ["card"],
-        customer_email: email,
-        mode: "payment",
-        success_url: "http://localhost:3000/checkout/success",
-        cancel_url: "http://localhost:3000",
-        line_items: lineItems,
-      });
-      // create the item
-      await strapi.service("api::order.order").create({
-        data: { userName, products, paystackSessionId: session.id },
-      });
-
-      //  return session id
-      return { id: session.id };
-    } catch (error) {
-      ctx.response.status = 500;
-      return { error: { message: "There was a problem creating the charge" } };
-    }
-  },
-}));
+module.exports = createCoreController("api::order.order");
