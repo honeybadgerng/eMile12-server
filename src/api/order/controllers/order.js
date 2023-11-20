0b90ea8e15fe97ef3bd2a8d2c59ad089f1c473a3
Moshood Raji <124494961+honeybadgerng@users.noreply.github.com>
2023-11-20 01:51:24 +0100

    Update order.js to collect orders
    this update works/comes with order controller and route

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,55 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::order.order");
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const {
+      billingAddress,
+      userName,
+      email,
+      phoneNumber,
+      products,
+      price_data,
+      price,
+    } = ctx.request.body;
+
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "ngn",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+
+      await strapi.service("api::order.order").create({
+        data: {
+          userName,
+          products,
+          billingAddress,
+          email,
+          phoneNumber,
+          price_data,
+          price,
+          lineItems,
+        },
+      });
+
+      return { message: "saved successfully" };
+    } catch (e) {
+      ctx.response.status = 500;
+      return { error: { message: "An Error occured " } };
+    }
+  },
+}));
